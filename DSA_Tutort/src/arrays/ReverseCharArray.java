22496f12629e34f055b47b25b78d0752edb931a8
Krishnendu Gupta <krishnendu.gupta00@gmail.com>
2022-11-24 15:07:05 +0530

    Initial Commit

---
+++ DSA_Tutort/src/arrays/ReverseCharArray.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class ReverseCharArray {
+
+    public static void main(String[] args) {
+
+        char[] arr = {'a','b','c','d'};
+        int beg = 0;
+        int end = arr.length - 1;
+
+        while (beg <end)
+        {
+            char t = arr[beg];
+            arr[beg] = arr[end];
+            arr[end] = t;
+            beg++;
+            end--;
+        }
+
+        for(char c: arr)
+            System.out.print(c+" ");
+
+    }
+}
