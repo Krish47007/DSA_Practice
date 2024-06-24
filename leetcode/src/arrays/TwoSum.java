828084f0f84393eef19fe4461b00ac7c8a12f1c7
Krishnendu Gupta <krish_007@Krishs-MBP.local>
2024-06-24 08:23:19 +0530

    2Sum problem

--- leetcode/src/arrays/TwoSum.java
+++ leetcode/src/arrays/TwoSum.java
@@ -1,5 +1,32 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class TwoSum {
 
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+
+            if(map.containsKey(target - nums[i])) {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+            }
+            else
+                map.put(nums[i],i);
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        for (int x : twoSum(new int[]{2,7,11,15},9))
+            System.out.print(x + " ");
+    }
 }
